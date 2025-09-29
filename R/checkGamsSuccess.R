fcec4682fde7d179a2fd0625211af233fad7b68b
ricardarosemann <ricarda.rosemann@pik-potsdam.de>
2025-09-29 15:27:25 +0200

    Read GAMS solver and model status
    Determine the most recent gdx from the main.lst and read model and solver status from this gdx.
    In standard brick run: Stop if there was a run error or the model is infeasible.
    In calibration run: If step size adaptation is infeasible, continue but make sure the respective step size is rejected. If any other run during the calibration is infeasible, stop but still write diagnostic output.

--- R/checkGamsSuccess.R
+++ R/checkGamsSuccess.R
@@ -1,38 +1,75 @@
 #' Check whether Gams finished successfully
 #'
-#' Check which output file was written and derive state of Gams run
+#' Check which file is the gdx file written by Gams from main.log.
+#' Read model and solver status from this gdx file and print.
 #'
-#' If \code{output.gdx} was written, give success message.
-#' If \code{abort.gdx} was written or no output file exists, stop with error message.
-#' For calibration runs:
-#' If any file with \code{calibration_<iteration>.gdx} was written, give success message
-#' including the number of iterations.
-#' If no file was written, stop with error message.
+#' Stop with error message if:
+#'   - \code{main.log} does not exist
+#'   - the gdx file does not exist
+#'   - more than one gdx file was written.
 #'
 #' @author Ricarda Rosemann
 #'
 #' @param path character, path to search for gams output files
-#' @param isCalibration logical, whether this is a calibration run
+#' @param silent logical, whether no messages should be printed.
 #'
-checkGamsSuccess <- function(path, isCalibration = FALSE) {
-  if (isTRUE(isCalibration)) {
-    allCalibrationFiles <- list.files(path, pattern = "calibration_\\d+\\.gdx$")
-    if (length(allCalibrationFiles) > 0) {
-      maxIteration <- max(as.numeric(sub("calibration_(\\d+)\\.gdx", "\\1", allCalibrationFiles)))
-      message("Gams successfully completed ", maxIteration, " calibration iterations: ",
-              "\"calibration_", maxIteration, ".gdx\" exists.")
+#' @returns logical, whether GAMS was successful for all subsets
+#'
+#' @importFrom dplyr %>% .data mutate
+#' @importFrom tidyr pivot_wider
+#'
+checkGamsSuccess <- function(path, silent = FALSE) {
+
+  .extractGdxPath <- function(lines, pattern) {
+    sub(paste0(".*", pattern, "\\s"), "", lines)
+  }
+
+  .checkModelRunStatus <- function(p_repy, desiredSolverStatus = 1, desiredModelStatus = c(1, 2)) {
+    p_repy %>%
+      pivot_wider(names_from = "solveinfo") %>%
+      mutate(success = .data$solvestat %in% desiredSolverStatus & .data$modelstat %in% desiredModelStatus) %>%
+      select(-any_of(c("solvestat", "modelstat", "resusd", "objval")))
+  }
+
+  # Check for gdx written in latest main.log
+  mainLogFile <- file.path(path, "main.log")
+  if (!file.exists(mainLogFile)) {
+    stop("Gams seems to not have run: 'main.log' does not exist")
+  }
+  mainLog <- readLines(mainLogFile)
+  gdxOutPattern <- "GDX File \\(execute_unload\\)"
+  gdxOutLines <- grep(gdxOutPattern, mainLog, value = TRUE)
+
+  gdxPath <- .extractGdxPath(gdxOutLines, gdxOutPattern)
+
+  if (length(gdxPath) == 0) {
+    stop("Gams did not write a GDX. This could be due to a compilation or execution error. ",
+         "Check the files 'main.log' and 'main.lst' for more details.")
+  } else if (length(gdxPath) == 1) {
+    if (file.exists(gdxPath)) {
+      message("Gams wrote a GDX file to ", gdxPath, " .")
     } else {
-      stop("Gams did not finish successfully.\n",
-           "Check the files main.log and main.lst for more information.")
+      stop("According to 'main.log', Gams wrote a GDX file to ", gdxPath, ", but this file does not exist.")
     }
   } else {
-    if (file.exists(file.path(path, "output.gdx"))) {
-      message("Gams was successful: \"output.gdx\" exists.")
-    } else if (file.exists(file.path(path, "abort.gdx"))) {
-      stop("Gams aborted with \"abort.gdx\" due to errors in at least one variable.")
-    } else {
-      stop("Gams did not finish successfully.\n",
-           "Check the files main.log and main.lst for more information.")
-    }
+    stop("Gams wrote more than one GDX file: ", gdxPath, " .")
   }
+
+  # Read out run status from gdx
+  p_repyFullSysLP <- readSymbol(gdxPath, symbol = "p_repyFullSysLP")
+  p_repyFullSysNLP <- readSymbol(gdxPath, symbol = "p_repyFullSysNLP")
+
+  if (isFALSE(silent)) {
+    message("Model and solver summary of the linear problem:")
+    print(p_repyFullSysLP)
+
+    message("Model and solver summary of the non-linear problem:")
+    print(p_repyFullSysNLP)
+  }
+
+  # Return success status if LP and NLP finished successfully
+  successLP <- .checkModelRunStatus(p_repyFullSysLP) # nolint:object_usage_linter
+  successNLP <- .checkModelRunStatus(p_repyFullSysNLP)
+
+  successNLP
 }
